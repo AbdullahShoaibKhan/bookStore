e8d186c906ca08f78dc00d8e6d63ba5b126f9a19
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2022-07-18 02:34:07 +0500

    commit

--- index.js
+++ index.js
@@ -10,9 +10,9 @@ app.use(cors());
 app.use("/books", router);
 if(process.env.NODE_ENV ==='production'){
     app.use(express.static('client/build'));
-    // app.get('*',(req,res)=>{
-    //     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    // })
+    app.get('*',(req,res)=>{
+        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    })
 }
 
 mongoose.connect('mongodb+srv://abdullah:236bscs@cluster0.mbzm0.mongodb.net/nodeTut?retryWrites=true&w=majority',
