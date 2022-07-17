f7a9fdf1a7d779ce26b58324f0c4748d587f2001
AbdullahShoaib <abdullahshoaib0333@gmail.com>
2022-07-18 02:14:38 +0500

    4th commit

--- index.js
+++ index.js
@@ -8,11 +8,11 @@ const path = require ("path");
 app.use(express.json());
 app.use(cors());
 app.use("/books", router);
-if(process.env.NODE_ENV==='production'){
-    app.use(express.static('client/build'))
-    app.get('*',(req,res)=>{
-        res.sendFile(path.resolve(__dirname,'client','build','index.html'));
-    })
+if(process.env.NODE_ENV ==='production'){
+    app.use(express.static('client/build'));
+    // app.get('*',(req,res)=>{
+    //     res.sendFile(path.resolve(__dirname,'client','build','index.html'));
+    // })
 }
 
 mongoose.connect('mongodb+srv://abdullah:236bscs@cluster0.mbzm0.mongodb.net/nodeTut?retryWrites=true&w=majority',
